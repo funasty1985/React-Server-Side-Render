c4e29c8f20bda2064801376cce29c4c76bf26f8b
funasty1985 <tony.f.liang@gmail.com>
2020-03-22 13:32:34 +0800

    20. Client Bootup
    - client.js is rewritten so that
            1. when the client visit '/' route and fetch the html script defined at the route, it will reach the <script> tag and ask the server for '/public/bundle.js'
            2. client.js is incorporated inside /public/bundle.js and will be execute.
            3. "ReactDOM.hydrate(<Home />, document.querySelector('#root'));" will look for the 'id=root' tag and insert the Home component in it
            4. Net effect of step1-3 :
                    Firstly, the client get the html script from the server where the skeleton of the <HOME/> is rendered (ie, without js, eventlistenor ...)
                    Secondly, the client fetch the bundle.js where the <HOME/> will be rendered , ie "breath to life or hydrated" without all js function ready.
                    Finally, the button on the client is clickable and return assigned function.
    
    Further note:
            1. <HOME/> will not be rendered completely again, but js function , listener ... will be inserted. This is referred to hydration
            2. We use ReactDOM.hydrate instead of ReactDOM.render because there is a hydration process but not a brand new render process .

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,3 +1,8 @@
 // Startup point for the clietn side application 
+// bootup locatio of the app in the browser 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home';
 
-console.log('Hi there from client.js')
+// ReactDOM.render(<Home />, document.querySelector('#root'));
+ReactDOM.hydrate(<Home />, document.querySelector('#root'));
