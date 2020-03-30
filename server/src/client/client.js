28d4e8971047b282efe32855bbc1bec0f788a406
funasty1985 <tony.f.liang@gmail.com>
2020-03-30 23:57:34 +0800

    31-39 Integrating Support for Redux
    - challenges for integration redux to ssr react app :
           1. Aspects of authentication needs to be handled on server.  Normally this is only on browser!
           2. Need some way to detect when all initial data load action creators (ie async call)are completed on server.
           3. Need state rehydration on the browser
      To over come 1-3, we will runs separate redux setting and config ,while share same set of action_creators and reducers, in server (src/index.js) and client (src/client/client.js)

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,14 +1,22 @@
 // Startup point for the client side application 
 // bootup locatio of the app in the browser 
 import React from 'react';
+import 'babel-polyfill';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import Routes from './Routes';
+import reducers from './reducers';
 
+const store = createStore(reducers, {}, applyMiddleware(thunk));  // second args of creatStore is the inital state
 
 // ReactDOM.render(<Home />, document.querySelector('#root'));
 ReactDOM.hydrate(
-    <BrowserRouter>
-        <Routes/>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes/>
+        </BrowserRouter>
+    </Provider>
     , document.querySelector('#root'));
--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,20 @@
+import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer'
+import createstore from './helpers/createStore';
+
 const app = express()
 
 app.use(express.static('public'))
 
 app.get('*', (req,res)=>{
-    res.send(renderer(req));
+    const store = createstore();
+
+    // Some logic to initialize 
+    // and load data into the store
+    // we don't do this insider the render 
+
+    res.send(renderer(req, store));
 });
 
 app.listen(3000,()=>{
